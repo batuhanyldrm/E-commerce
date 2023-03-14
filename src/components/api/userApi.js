d06bf7fa4b8460bd9ec92c629f83bb49a2756a98
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-03-14 16:17:06 +0300

    deleted some codes

--- src/components/api/userApi.js
+++ src/components/api/userApi.js
@@ -13,10 +13,3 @@ export const postRegister = async (data) => {
 
     return resp
 }
-
-
-
-export const getProducts = async () => {
-    const resp = await axios.get("http://localhost:3001/stocks")
-    return resp;
-}
