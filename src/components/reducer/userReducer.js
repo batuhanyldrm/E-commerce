a3b52b760accac9304bad39dd497200af6a6b347
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-03-15 16:26:10 +0300

    create user action and reducer

---
+++ src/components/reducer/userReducer.js
@@ -0,0 +1,14 @@
+import { ADD_USER } from "../actions/types";
+
+const UserReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ADD_USER:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+export default UserReducer;
