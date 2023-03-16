3eb060904e1e7e4daa041b7979c0464f98832277
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-03-16 19:11:19 +0300

    add api's

--- src/components/api/userApi.js
+++ src/components/api/userApi.js
@@ -13,3 +13,31 @@ export const postRegister = async ({name, surname, email, password, tel, company
 
     return resp.status === 201 ? resp : false
 }
+
+export const postLogin = async ({name, surname, email, password, tel, company, role}) => {
+    const resp = await axios.post("http://localhost:3001/login", {
+        name: name,
+        surname: surname,
+        email: email,
+        password: password,
+        tel: tel,
+        /* company: company,
+        role: role, */
+    })
+
+    return resp.status === 201 ? resp : false
+}
+
+export const postLogout = async ({name, surname, email, password, tel, company, role}) => {
+    const resp = await axios.post("http://localhost:3001/login", {
+        name: name,
+        surname: surname,
+        email: email,
+        password: password,
+        tel: tel,
+        /* company: company,
+        role: role, */
+    })
+
+    return resp.status === 201 ? resp : false
+}
