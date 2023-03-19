e109e57aae692e5594cfbadb3a6529ea4d12a105
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-03-19 20:43:32 +0300

    change router url

--- src/components/company/RouterPage.js
+++ src/components/company/RouterPage.js
@@ -8,7 +8,7 @@ const RouterPage = () => {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Page component={LoginPage} title={"Home"} />}></Route>
+          <Route path="/" element={<LoginPage component={LoginPage} title={"Home"} />}></Route>
           <Route path="/register" element={<LoginPage component={LoginPage} title={"Login"} />}></Route>
           <Route exact path="/stock" element={<Page component={Product} title={"Mevcut Stoklar"} />}></Route>
         </Routes>
