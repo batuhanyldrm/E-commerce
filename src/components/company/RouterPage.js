7c89d7a0f3d21b8d714b3156144dd51b752df0b5
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-03-24 16:35:53 +0300

    add all products page

--- src/components/company/RouterPage.js
+++ src/components/company/RouterPage.js
@@ -3,13 +3,14 @@ import React from 'react'
 import Product from "../Product";
 import Page from "../common/Page";
 import LoginPage from "../LoginPage";
+import AllProducts from "../AllProducts";
 
 const RouterPage = () => {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage component={LoginPage} title={"Home"} />}></Route>
-          <Route path="/register" element={<LoginPage component={LoginPage} title={"Login"} />}></Route>
+          <Route path="/allProducts" element={<AllProducts component={LoginPage} title={"AllProducts"} />}></Route>
           <Route exact path="/stock" element={<Page component={Product} title={"Mevcut Stoklar"} />}></Route>
         </Routes>
           
