55cb30dc7134e190805f7ae5229787e054e5c7d5
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-05-10 15:52:39 +0300

    change login page and default url

--- src/components/company/RouterPage.js
+++ src/components/company/RouterPage.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import React from 'react'
 import Product from "../Product";
 import Page from "../common/Page";
@@ -9,7 +9,10 @@ const RouterPage = () => {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginPage component={LoginPage} title={"Home"} />}></Route>
+        <Route>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<LoginPage component={LoginPage} title={"Home"} />}></Route>
+          </Route>
           <Route path="/allProducts" element={<AllProducts component={LoginPage} title={"AllProducts"} />}></Route>
           <Route exact path="/stock" element={<Page component={Product} title={"Mevcut Stoklar"} />}></Route>
         </Routes>
