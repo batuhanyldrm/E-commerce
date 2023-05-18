746ea62b7b6eeaabb50371cb55d5e92850a9913b
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-05-18 23:38:01 +0300

    add detail page fetch product

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { makeStyles } from '@mui/styles';
+import { fetchProducts } from './actions/productActions';
+import { connect } from 'react-redux';
 
-const ProductDetails = () => {
+const useStyles = makeStyles((theme) => ({
+  root:{
+    marginLeft:"5px", 
+    minWidth:200, 
+    maxWidth:250, 
+    minHeight:300, 
+    border:'1px solid lightgray', 
+    borderRadius:5
+  }
+}));
+
+const ProductDetails = (props) => {
+  const classes = useStyles();
+
+  const {
+    products,
+    fetchProducts
+  } = props;
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+  
   return (
-    <div>ProductDetails</div>
+    <div className={classes.root}>
+    </div>
   )
 }
 
-export default ProductDetails
+const mapStateToProps = (state) => ({
+  products: state.products.products
+});
+
+const mapDispatchToProps = (dispatch) => ({
+fetchProducts: () => {
+  dispatch(fetchProducts());
+},
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)  (ProductDetails)
