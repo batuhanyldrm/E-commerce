8f02e0637f39d0cb9ae09bca19d8e815fe2823e4
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-05-20 22:13:14 +0300

    fix product detail page error

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,48 +1,87 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@mui/styles';
-import { fetchProduct } from './actions/productActions';
 import { connect } from 'react-redux';
+import { fetchProduct } from './actions/productActions';
 
 const useStyles = makeStyles((theme) => ({
-  root:{
-    marginLeft:"5px", 
-    minWidth:200, 
-    maxWidth:250, 
-    minHeight:300, 
-    border:'1px solid lightgray', 
-    borderRadius:5
-  }
+  root: {
+    marginLeft: '5px',
+    maxWidth: 600,
+    maxHeight: 400,
+    border: '1px solid lightgray',
+    borderRadius: 5,
+  },
+  image: {
+    margin: '5px',
+    minWidth: 200,
+    maxWidth: 250,
+    minHeight: 300,
+    border: '1px solid lightgray',
+    borderRadius: 5,
+  },
 }));
 
 const ProductDetails = (props) => {
   const classes = useStyles();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-  const {
-    products,
-    fetchProduct
-  } = props;
+  const { products, fetchProduct } = props;
 
+  //tekrar kontrol edilecek
   useEffect(() => {
-    fetchProduct(window.location.href.split("/")[4])
-  }, [])
+    const productId = window.location.href.split('/')[4];
+    fetchProduct(productId);
 
-  console.log(window.location.href.split("/")[4])
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 200);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [fetchProduct]);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  const handleImageError = () => {
+    setIsLoading(false);
+    setIsError(true);
+  };
 
   return (
-    <div >
-      <img className={classes.root} /* src={products.image} */></img>
+    <div className={classes.root}>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error loading image</div>}
+      {!isLoading && !isError && (
+        <div style={{ display: "flex" }}>
+          <img
+            className={classes.image}
+            src={products.image}
+            alt="Product"
+            onLoad={handleImageLoad}
+            onError={handleImageError}
+          />
+          <div style={{ marginLeft: "10px" }}>
+            <p>{products.productName}</p>
+            <p>{products.description}</p>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => ({
-  products: state.products.products
+  products: state.products.products,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-fetchProduct: (id) => {
-  dispatch(fetchProduct(id));
-},
+  fetchProduct: (id) => {
+    dispatch(fetchProduct(id));
+  },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)  (ProductDetails)
+export default connect(mapStateToProps, mapDispatchToProps)(ProductDetails);
