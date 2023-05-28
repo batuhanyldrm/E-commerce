4bb97c2091bba246de797916249893f84cc209b1
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-05-28 21:26:51 +0300

    fix product detail page ui

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,18 +3,23 @@ import { makeStyles } from '@mui/styles';
 import { connect } from 'react-redux';
 import { fetchProduct } from './actions/productDetailActions';
 import ResponsiveAppBar from './ResponsiveAppBar';
-import { Button } from '@mui/material';
+import { Button, CircularProgress } from '@mui/material';
 import CameraAltIcon from '@material-ui/icons/CameraAlt';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginLeft: '5px',
     maxWidth: 600,
-    maxHeight: 400,
+    //maxHeight: 400,
     border: '1px solid lightgray',
     borderRadius: 5,
     ["@media (max-width: 400px)"]:{
-      display:"flex",
+    },
+  },
+  flex: {
+    display:"flex",
+    ["@media (max-width: 400px)"]:{
+      display:"grid",
     },
   },
   image: {
@@ -49,73 +54,79 @@ const useStyles = makeStyles((theme) => ({
     "&:hover": {
       opacity:0.8
     },
-  }
+  },
+  loadingContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 250,
+    height: 200,
+  },
 }));
 
 const ProductDetails = (props) => {
   const { productDetail, fetchProduct } = props;
 
   const classes = useStyles();
-
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-/*   const [cartItems, setCartItems] = useState([] || "");
 
-  const handleAddToCart = (product) => {
-    setCartItems([...cartItems, product]);
-  };
-  console.log(cartItems,"www") */
-
-  //tekrar kontrol edilecek
   useEffect(() => {
     const productId = window.location.href.split('/')[4];
     fetchProduct(productId);
+  }, [fetchProduct]);
 
-    const timeoutId = setTimeout(() => {
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
       setIsLoading(false);
     }, 200);
 
     return () => {
-      clearTimeout(timeoutId);
+      clearTimeout(loadingTimeout);
     };
-  }, [fetchProduct]);
-
-  const handleImageLoad = () => {
-    setIsLoading(false);
-  };
-
-  const handleImageError = () => {
-    setIsLoading(false);
-    setIsError(true);
-  };
+  }, []);
 
   return (
     <>
-      <ResponsiveAppBar/>
-      <div className={classes.root} style={{margin:"auto"}}>
-        {isLoading && <div>Loading...</div>}
-        {isError && <div>Error loading image</div>}
-        {!isLoading && !isError && (
-          <div style={{ display: "flex" }}>
-            {productDetail.image ?
-              <img
-                className={classes.listImgBlock}
-                src={productDetail.image}
-                alt="Product"
-                onLoad={handleImageLoad}
-                onError={handleImageError}
-              /> : <CameraAltIcon className={classes.listImgBlock} />
-            }
-            <div style={{ marginLeft: "10px" }}>
-              <p>Product Name:  {productDetail.productName}</p>
-              <p>Product Details: {productDetail.description}</p>
-              <p>Price: {productDetail.price}</p>
-              <p>Stock: {productDetail.amount}</p>
-              <Button variant="contained" className={classes.btn} style={{minWidth:100, backgroundColor:"rgba(39,38,152,255)"}}>Add to Cart</Button>
-              <Button variant="contained" color="primary" className={classes.btn} style={{marginLeft:5 ,minWidth:100, backgroundColor:"rgba(186,130,57,255)"}}>Buy Now</Button>
-            </div>
+      <ResponsiveAppBar />
+      <div className={classes.root} style={{ margin: 'auto' }}>
+        <div className={classes.flex}>
+          {isLoading ? (
+            <CircularProgress className={classes.loadingContainer} />
+          ) : (
+            <>
+              {productDetail.image ? (
+                <img
+                  className={classes.listImgBlock}
+                  src={productDetail.image}
+                  alt="Product"
+                />
+              ) : (
+                <CameraAltIcon className={classes.listImgBlock} />
+              )}
+            </>
+          )}
+          <div style={{ marginLeft: '10px' }}>
+            <p>Product Name: {productDetail.productName}</p>
+            <p>Product Details: {productDetail.description}</p>
+            <p>Price: {productDetail.price}</p>
+            <p>Stock: {productDetail.amount}</p>
+            <Button
+              variant="contained"
+              className={classes.btn}
+              style={{ minWidth: 100, backgroundColor: 'rgba(39,38,152,255)' }}
+            >
+              Add to Cart
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.btn}
+              style={{ marginLeft: 5, minWidth: 100, backgroundColor: 'rgba(186,130,57,255)' }}
+            >
+              Buy Now
+            </Button>
           </div>
-        )}
+        </div>
       </div>
     </>
   );
