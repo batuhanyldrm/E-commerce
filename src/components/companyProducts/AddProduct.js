592200a46174931489b4a4e855b915b7292bc9fc
batuhan yıldırım <yildirimbatu52@gmail.com>
2024-01-10 01:08:25 +0300

    fix code spaces

--- src/components/companyProducts/AddProduct.js
+++ src/components/companyProducts/AddProduct.js
@@ -28,116 +28,116 @@ const useStyles = makeStyles((theme) => ({
 function AddProduct(props) {
   const classes = useStyles();
 
-    const {open, handleClose, addProduct} = props
+  const {open, handleClose, addProduct} = props
 
-    const [productName, setProductName] = useState("")
-    const [description, setDescription] = useState("")
-    const [price, setPrice] = useState(0)
-    const [amount, setAmount] = useState(0)
-    const [image, setImage] = useState(null)
+  const [productName, setProductName] = useState("")
+  const [description, setDescription] = useState("")
+  const [price, setPrice] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [image, setImage] = useState(null)
 
-const handleCreateProduct = async () => {
-  const formData = new FormData();
-  formData.append('productName', productName);
-  formData.append('description', description);
-  formData.append('price', price);
-  formData.append('amount', amount);
-  formData.append('image', image[0]);
-  
-  try {
-    const res = await postProduct(formData);
-    addProduct(res.data);
-  } catch (error) {
-  } finally {
-    handleClose(false);
-  }
-};
+  const handleCreateProduct = async () => {
+    const formData = new FormData();
+    formData.append('productName', productName);
+    formData.append('description', description);
+    formData.append('price', price);
+    formData.append('amount', amount);
+    formData.append('image', image[0]);
+    
+    try {
+      const res = await postProduct(formData);
+      addProduct(res.data);
+    } catch (error) {
+    } finally {
+      handleClose(false);
+    }
+  };
 
-    return(
+  return(
     <div>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-          fullWidth
-        >
-          <DialogTitle id="alert-dialog-title">
-            <div style={{ textAlign : "center" }}>
-              Add Stock
-            </div>
-          </DialogTitle>
-          <DialogContent>
-          <form>
-            <div className={classes.root}>
-          <TextField
-            id="product"
-            value={productName}
-            onChange={(e) => setProductName(e.target.value)}
-            type="text"
-            margin="normal"
-            label="Product Name"
-            variant="outlined"
-            size='small'
-          />
-          <TextField
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            autoFocus
-            margin="normal"
-            id="description"
-            label="Description"
-            type="text"
-            variant="outlined"
-            size='small'
-          />
-          <TextField
-            value={amount}
-            onChange={(e) => setAmount(parseInt(e.target.value))}
-            autoFocus
-            margin="normal"
-            id="amount"
-            label="Amount"
-            type="number"
-            variant="outlined"
-            size='small'
-          />
-          <TextField
-            value={price}
-            onChange={(e) => setPrice(parseInt(e.target.value))}
-            autoFocus
-            margin="normal"
-            id="price"
-            label="Price"
-            type="number"
-            variant="outlined"
-            size='small'
-          />
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        fullWidth
+      >
+        <DialogTitle id="alert-dialog-title">
+          <div style={{ textAlign : "center" }}>
+            Add Stock
           </div>
-          <Button
-            variant="contained"
-            component="label"
-            color={"primary"}
-          >
-            <div className={classes.imgbutton}>Add Image</div>
-              <BackupIcon />
-            <input
-              type="file"
-              accept="image/*"
-              onChange={(e) => setImage(e.target.files)}
-              id={"document-upload-button"}
-              style={{ zIndex: "-1", width: "0px", height: "1px" }}
-            />
-          </Button>
-          </form>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={() => handleCreateProduct()}>ADD</Button>
-          </DialogActions>
+        </DialogTitle>
+        <DialogContent>
+        <form>
+          <div className={classes.root}>
+        <TextField
+          id="product"
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+          type="text"
+          margin="normal"
+          label="Product Name"
+          variant="outlined"
+          size='small'
+        />
+        <TextField
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          autoFocus
+          margin="normal"
+          id="description"
+          label="Description"
+          type="text"
+          variant="outlined"
+          size='small'
+        />
+        <TextField
+          value={amount}
+          onChange={(e) => setAmount(parseInt(e.target.value))}
+          autoFocus
+          margin="normal"
+          id="amount"
+          label="Amount"
+          type="number"
+          variant="outlined"
+          size='small'
+        />
+        <TextField
+          value={price}
+          onChange={(e) => setPrice(parseInt(e.target.value))}
+          autoFocus
+          margin="normal"
+          id="price"
+          label="Price"
+          type="number"
+          variant="outlined"
+          size='small'
+        />
+        </div>
+        <Button
+          variant="contained"
+          component="label"
+          color={"primary"}
+        >
+          <div className={classes.imgbutton}>Add Image</div>
+            <BackupIcon />
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setImage(e.target.files)}
+            id={"document-upload-button"}
+            style={{ zIndex: "-1", width: "0px", height: "1px" }}
+          />
+        </Button>
+        </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={() => handleCreateProduct()}>ADD</Button>
+        </DialogActions>
       </Dialog>
     </div>
-    );
+  );
 }
 
 const mapStateToProps = (state) => ({
