b363734eb739cab808e1147498d47264bec13d31
batuhan yıldırım <yildirimbatu52@gmail.com>
2024-06-06 11:48:53 +0300

    order table header crated

--- src/components/companyProducts/Orders.js
+++ src/components/companyProducts/Orders.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
 import { makeStyles } from '@mui/styles';
 import { fetchOrders } from '../actions/orderActions';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -30,6 +31,30 @@ function Orders(props) {
 
     return(
     <div>
+      {/* <TableContainer component={Paper}> */}
+        <Table component={Paper} sx={{ minWidth: 5, marginTop:2, maxWidth:1150 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Product Name</TableCell>
+              <TableCell>Product Code</TableCell>
+              <TableCell align="left">Address</TableCell>
+              <TableCell align="right">Total Price</TableCell>
+              <TableCell align="right">Payment</TableCell>
+              <TableCell align="right">Discount</TableCell>
+              <TableCell align="right">Additional Note</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* {products.products && products.products.map((product, index) => (
+              <ProductListItem
+              product={product}
+              index={index}
+              key={product.id + "" + index}
+              />
+            ))} */}
+          </TableBody>
+        </Table>
+      {/* </TableContainer> */}
     </div>
     );
 }
